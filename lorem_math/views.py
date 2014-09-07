123ff49b6cf09f86551881db92000f62a2c67411
Rohan Das <das.rohan1993@gmail.com>
2014-09-06 23:58:17 -0400

    Working version 1.0

--- lorem_math/views.py
+++ lorem_math/views.py
@@ -8,7 +8,9 @@ def index(request):
 	context = {'formula': r.formula}
 	return render_to_response('index.html', context)
 
-def random_latex(request):
-	r = RandomFormula()
-	context['formula'] = r.formula
-	return render_to_response('index.html', context)
+
+''' For when we want to load the LaTeX asynchronously or on a different page etc '''
+# def random_latex(request):
+# 	r = RandomFormula()
+# 	context['formula'] = r.formula
+# 	return render_to_response('index.html', context)
