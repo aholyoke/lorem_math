f3ea542a014d5686524225102b66544988bc99ee
Alex Holyoke <aholyoke@uwaterloo.ca>
2014-09-07 11:35:14 -0400

    Added raw view with num_equal_signs as query parameter

--- lorem_math/urls.py
+++ lorem_math/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'lorem_math.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^/*$', 'lorem_math.views.index'),
-    # url(r'^random/*$', 'lorem_math.views.random_latex')
+    url(r'^random/*$', 'lorem_math.views.random_latex')
 
 )
--- lorem_math/views.py
+++ lorem_math/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render_to_response, render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 
-from random_latex import *
+from random_latex import RandomFormula
+
 
 def index(request):
-	r = RandomFormula()
-	context = {'formula': r.formula}
-	return render_to_response('index.html', context)
+    r = RandomFormula()
+    context = {'formula': r.formula}
+    return render_to_response('index.html', context)
 
 
-''' For when we want to load the LaTeX asynchronously or on a different page etc '''
-# def random_latex(request):
-# 	r = RandomFormula()
-# 	context['formula'] = r.formula
-# 	return render_to_response('index.html', context)
+def random_latex(request):
+    num_equal_signs = int(request.GET.get('num_equal_signs', '0'))
+    r = RandomFormula(num_equal_signs)
+    context = {'formula': r.formula}
+    return render_to_response('raw_page.html', context)
