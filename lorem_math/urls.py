230a4fb713975fcadff12c3976096e8454ca08da
Rohan Das <das.rohan1993@gmail.com>
2014-09-06 23:53:24 -0400

    Raw LaTeX randomly generated and displayed, not rendered though

--- lorem_math/urls.py
+++ lorem_math/urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^/*$', 'lorem_math.views.index'),
+    url(r'^random/*$', 'lorem_math.views.random_latex')
 
 )
--- lorem_math/views.py
+++ lorem_math/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render_to_response, render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 
+from random_latex import *
 
 def index(request):
-	return render_to_response('index.html')
+	r = RandomFormula()
+	context = {'formula': r.formula}
+	return render_to_response('index.html', context)
+
+def random_latex(request):
+	r = RandomFormula()
+	context['formula'] = r.formula
+	return render_to_response('index.html', context)
